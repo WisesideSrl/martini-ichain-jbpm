8a9b5002189c57da440934ead4333fa398921fb4
wbadmin <>
2025-11-24 17:23:34 +0000

    {/src/main/java/com/wiseside/martinidev/model/CicloAllevamento.java}

--- src/main/java/com/wiseside/martinidev/model/CicloAllevamento.java
+++ src/main/java/com/wiseside/martinidev/model/CicloAllevamento.java
@@ -3,15 +3,42 @@ package com.wiseside.martinidev.model;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-public class CicloAllevamento implements Serializable{
-    private static final long serialVersionUID=-1;
+public class CicloAllevamento implements Serializable {
+	private static final long serialVersionUID = -1;
 
-    public String codiceCiclo;
+	public String codiceCiclo;
 
-    public LocalDate dataInizioPianificata;
+	public LocalDate dataInizioPianificata;
 
-    public Integer numeroMaschi;
+	public Integer numeroMaschi;
 
-    public Integer numeroFemmine;
+	public Integer numeroFemmine;
+
+	private java.util.List<com.wiseside.martinidev.model.Accasamento> accasamenti;
+
+	public java.util.List<com.wiseside.martinidev.model.Accasamento> getAccasamenti() {
+		return this.accasamenti;
+	}
+
+	public void setAccasamenti(
+			java.util.List<com.wiseside.martinidev.model.Accasamento> accasamenti) {
+		this.accasamenti = accasamenti;
+	}
+
+	public CicloAllevamento() {
+	}
+
+	public CicloAllevamento(
+			java.lang.String codiceCiclo,
+			java.time.LocalDate dataInizioPianificata,
+			java.lang.Integer numeroMaschi,
+			java.lang.Integer numeroFemmine,
+			java.util.List<com.wiseside.martinidev.model.Accasamento> accasamenti) {
+		this.codiceCiclo = codiceCiclo;
+		this.dataInizioPianificata = dataInizioPianificata;
+		this.numeroMaschi = numeroMaschi;
+		this.numeroFemmine = numeroFemmine;
+		this.accasamenti = accasamenti;
+	}
 
 }
